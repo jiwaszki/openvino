11592a644b4e74587a21e5938579848e0ee01611
jiwaszki <jan.iwaszkiewicz@intel.com>
2020-07-20 13:41:31 +0200

    worksave

--- ngraph/python/tests/test_ngraph/test_manager.py
+++ ngraph/python/tests/test_ngraph/test_manager.py
@@ -17,28 +17,29 @@
 import numpy as np
 
 import ngraph as ng
-from ngraph.impl import AxisSet, Function, Shape, Type
+from ngraph.impl import AxisSet, Function, Shape, Type, Manager
 from ngraph.impl.op import Constant, Parameter
 from tests.runtime import get_runtime
 
 
 def test_constant_folding_ceiling():
-    auto constant = op::Constant::create(
-        element::f32, Shape{2, 3}, vector<float>{0.0f, 0.1f, -0.1f, -2.5f, 2.5f, 3.0f});
-    auto ceil = make_shared<op::Ceiling>(constant);
-    auto f = make_shared<Function>(ceil, ParameterVector{});
+    input_array = [0.0, 0.1, -0.1, -2.5, 2.5, 3.0]
+    node_constant = ng.constant(input_array, dtype=np.float32) # TODO: Shape{2, 3}
+    node_ceil = ng.ceiling(constant)
+    func = Function(node_ceil, ParameterVector{}) # TODO: ParameterVector
+    # result = run_op_node(input_array, ng_api_fn)
 
-    pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ConstantFolding>();
-    pass_manager.run_passes(f);
+    pass_manager = Manager()
+    pass_manager.register_pass<ConstantFolding>()
+    pass_manager.run_passes(f)
 
-    ASSERT_EQ(count_ops_of_type<op::Ceiling>(f), 0);
-    ASSERT_EQ(count_ops_of_type<op::Constant>(f), 1);
+    assert count_ops_of_type<op::Ceiling>(f) == 0
+    assert count_ops_of_type<op::Constant>(f) == 1
 
     auto new_const = as_type_ptr<op::Constant>(f->get_results().at(0)->get_argument(0));
-    ASSERT_TRUE(new_const);
-    auto values_out = new_const->get_vector<float>();
+    assert new_const
+    auto values_out = new_const.get_vector();
 
-    vector<float> values_expected{0.0f, 1.0f, 0.0f, -2.0f, 3.0f, 3.0f};
+    values_expected = [0.0, 1.0, 0.0, -2.0, 3.0, 3.0]
 
-    ASSERT_TRUE(test::all_close_f(values_out, values_expected, MIN_FLOAT_TOLERANCE_BITS));
+    assert np.all_close(values_out, values_expected)
