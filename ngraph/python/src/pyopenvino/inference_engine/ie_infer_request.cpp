1566e3a73d258c5157a6e2d86f0a5b4db00c60d1
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-04-14 02:31:34 +0200

    replace InferRequest with wrapper and update infer methods

--- ngraph/python/src/pyopenvino/inference_engine/ie_infer_request.cpp
+++ ngraph/python/src/pyopenvino/inference_engine/ie_infer_request.cpp
@@ -2,89 +2,92 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <pybind11/functional.h>
-#include <pybind11/stl.h>
 
 #include <string>
-#include <vector>
 
-#include <cpp/ie_infer_request.hpp>
 #include <ie_common.h>
 
 #include "pyopenvino/inference_engine/common.hpp"
 #include "pyopenvino/inference_engine/ie_executable_network.hpp"
 #include "pyopenvino/inference_engine/ie_infer_request.hpp"
 #include "pyopenvino/inference_engine/ie_preprocess_info.hpp"
+#include "pyopenvino/inference_engine/containers.hpp"
 
 namespace py = pybind11;
 
 void regclass_InferRequest(py::module m)
 {
-    py::class_<InferenceEngine::InferRequest, std::shared_ptr<InferenceEngine::InferRequest>> cls(
+    py::class_<InferRequestWrapper, std::shared_ptr<InferRequestWrapper>> cls(
         m, "InferRequest");
 
-    cls.def("set_batch", &InferenceEngine::InferRequest::SetBatch, py::arg("size"));
+    cls.def("set_batch", [](InferRequestWrapper& self, const int size) {
+        self.SetBatch(size);
+    }, py::arg("size"));
 
-    cls.def("get_blob", &InferenceEngine::InferRequest::GetBlob);
+    cls.def("get_blob", [](InferRequestWrapper& self, const std::string& name) {
+        return self.GetBlob(name);
+    }, py::arg("name"));
 
-    cls.def("set_blob",
-            [](InferenceEngine::InferRequest& self, const std::string& name, py::handle blob) {
-                self.SetBlob(name, Common::cast_to_blob(blob));
-            });
+    cls.def("set_blob", [](InferRequestWrapper& self,
+                           const std::string& name,
+                           py::handle& blob) {
+        self.SetBlob(name, Common::cast_to_blob(blob));
+    }, py::arg("name"), py::arg("blob"));
 
-    cls.def("set_input", [](InferenceEngine::InferRequest& self, const py::dict& inputs) {
-        for (auto&& input : inputs) {
-            auto name = input.first.cast<std::string>();
-            auto blob = Common::cast_to_blob(input.second);
-            self.SetBlob(name, blob);
+    cls.def("set_blob", [](InferRequestWrapper& self,
+                           const std::string& name,
+                           py::handle& blob,
+                           const InferenceEngine::PreProcessInfo& info) {
+        self.SetBlob(name, Common::cast_to_blob(blob));
+    }, py::arg("name"), py::arg("blob"), py::arg("info"));
+
+    cls.def("set_input", [](InferRequestWrapper& self, const py::dict& inputs) {
+        Common::set_request_blobs(self, inputs);
+    }, py::arg("inputs"));
+
+    cls.def("set_output", [](InferRequestWrapper& self, const py::dict& results) {
+        Common::set_request_blobs(self, results);
+    }, py::arg("results"));
+
+    cls.def("_infer", [](InferRequestWrapper& self, const py::dict& inputs) {
+        // Update inputs if there are any
+        if (!inputs.empty()) {
+            Common::set_request_blobs(self, inputs);
         }
-    });
-
-    cls.def("set_output", [](InferenceEngine::InferRequest& self, const py::dict& results) {
-        for (auto&& result : results) {
-            auto name = result.first.cast<std::string>();
-            auto blob = Common::cast_to_blob(result.second);
-            self.SetBlob(name, blob);
+        // Call Infer function
+        self.Infer();
+        // Get output Blobs and return
+        Containers::PyResults results;
+        for (auto& out : self._outputsInfo)
+        {
+            results[out.first] = self.GetBlob(out.first);
         }
-    });
+        return results;
+    }, py::arg("inputs"));
 
-    cls.def("infer", &InferenceEngine::InferRequest::Infer);
-
-    cls.def("async_infer", [](InferenceEngine::InferRequest& self) {
+    cls.def("async_infer", [](InferRequestWrapper& self) {
         py::gil_scoped_release release;
         self.StartAsync();
     });
 
-    cls.def("set_blob", [](InferenceEngine::InferRequest& self,
-                           const std::string& name,
-                           py::handle blob) {
-        self.SetBlob(name,  Common::cast_to_blob(blob));
-    });
-
-    cls.def("set_blob", [](InferenceEngine::InferRequest& self,
-                           const std::string& name,
-                           py::handle blob,
-                           const InferenceEngine::PreProcessInfo& info) {
-        self.SetBlob(name, Common::cast_to_blob(blob));
-    });
-
     cls.def(
         "wait",
-        [](InferenceEngine::InferRequest& self, int64_t millis_timeout) {
+        [](InferRequestWrapper& self, int64_t millis_timeout) {
             py::gil_scoped_acquire acquire;
             return self.Wait(millis_timeout);
         },
         py::arg("millis_timeout") = InferenceEngine::IInferRequest::WaitMode::RESULT_READY);
 
     cls.def("set_completion_callback",
-            [](InferenceEngine::InferRequest& self, py::function f_callback) {
+            [](InferRequestWrapper& self, py::function f_callback) {
                 self.SetCompletionCallback([self, f_callback]() {
                     // py::gil_scoped_acquire acquire;
                     f_callback(self);
                     // py::gil_scoped_release release;
                 });
-            });
+            }, py::arg("f_callback"));
 
-    cls.def("get_perf_counts", [](InferenceEngine::InferRequest& self) {
+    cls.def("get_perf_counts", [](InferRequestWrapper& self) {
         std::map<std::string, InferenceEngine::InferenceEngineProfileInfo> perfMap;
         perfMap = self.GetPerformanceCounts();
         py::dict perf_map;
@@ -115,17 +118,35 @@ void regclass_InferRequest(py::module m)
         return perf_map;
     });
 
-    cls.def("preprocess_info", &InferenceEngine::InferRequest::GetPreProcess, py::arg("name"));
+    cls.def("preprocess_info", [](InferRequestWrapper& self, const std::string& name) {
+        return self.GetPreProcess(name);
+    }, py::arg("name"));
 
-    //    cls.def_property_readonly("preprocess_info", [](InferenceEngine::InferRequest& self) {
+    //    cls.def_property_readonly("preprocess_info", [](InferRequestWrapper& self) {
     //
     //    });
-    //    cls.def_property_readonly("input_blobs", [](){
-    //
-    //    });
-    //    cls.def_property_readonly("output_blobs", [](){
-    //
-    //    });
+
+    cls.def_property_readonly("input_blobs", [](InferRequestWrapper& self) {
+        Containers::PyResults input_blobs;
+        for (auto& in : self._inputsInfo)
+        {
+            input_blobs[in.first] = self.GetBlob(in.first);
+        }
+        return input_blobs;
+    });
+
+    cls.def_property_readonly("output_blobs", [](InferRequestWrapper& self) {
+        Containers::PyResults output_blobs;
+        for (auto& out : self._outputsInfo)
+        {
+            output_blobs[out.first] = self.GetBlob(out.first);
+        }
+        return output_blobs;
+    });
+
+    // cls.def("__del__", [](InferRequestWrapper& self) {
+    //     InferenceEngine::InferRequest::actual = nullptr;
+    // });
 
     //    latency
 }
--- ngraph/python/src/pyopenvino/inference_engine/ie_infer_request.hpp
+++ ngraph/python/src/pyopenvino/inference_engine/ie_infer_request.hpp
@@ -6,8 +6,20 @@
 
 #include <pybind11/pybind11.h>
 
+#include <cpp/ie_executable_network.hpp>
 #include <cpp/ie_infer_request.hpp>
+#include <ie_input_info.hpp>
 
 namespace py = pybind11;
+
+class InferRequestWrapper : public InferenceEngine::InferRequest {
+public:
+    using InferenceEngine::InferRequest::InferRequest;
+
+    // ~InferRequestWrapper() = default;
+
+    InferenceEngine::ConstInputsDataMap _inputsInfo;
+    InferenceEngine::ConstOutputsDataMap _outputsInfo;
+};
 
 void regclass_InferRequest(py::module m);
