1ae6b23040c3fff771b38d073382787fa79e3e89
jiwaszki <jan.iwaszkiewicz@intel.com>
2021-04-19 10:25:01 +0200

    Draft for queue example with database

--- ngraph/python/examples/queue_example.py
+++ ngraph/python/examples/queue_example.py
@@ -7,7 +7,9 @@ The Inference Queue allows him to run inference as parallel jobs.
 """
 
 import numpy as np
+import pandas as pd
 import time
+import sqlalchemy as db
 
 from openvino.inference_engine import IECore
 # from openvino.inference_engine import TensorDesc
@@ -23,6 +25,24 @@ def get_reference(executable_network, images):
     return [executable_network.infer({'data': img}) for img in images]
 
 
+def show_table(tab):
+    results = connection.execute(db.select([tab])).fetchall()
+    if len(results) != 0:
+        df = pd.DataFrame(results)
+        df.columns = results[0].keys()
+        print(df.head(6))
+
+
+engine = db.create_engine('sqlite:///queue.sqlite')
+connection = engine.connect()
+metadata = db.MetaData()
+
+emp = db.Table('emp', metadata,
+               db.Column('Id', db.Integer()),
+               db.Column('pred_class', db.Integer()))
+
+metadata.create_all(engine)  # Creates the table
+
 # Read images from a folder
 images = get_images()
 
@@ -37,41 +57,53 @@ executable_network = ie.load_network(network=ie_network,
 
 
 # Create InferQueue with specific number of jobs/InferRequests
-infer_queue = InferQueue(network=executable_network, jobs=6)
+infer_queue = InferQueue(network=executable_network, jobs=3)
 
 executable_network.infer()
 
+ref_results = []
+
 ref_start_time = time.time()
-for img in images:
-    res = executable_network.infer({'data': img})
+for i in range(len(images)):
+    res = executable_network.infer({'data': images[i]})
+    pred_class = np.argmax(res['fc_out'])
+    ref_results += [(i, pred_class)]
 ref_end_time = time.time()
 ref_time = (ref_end_time - ref_start_time) * 1000
-
-results = [0] * len(images)
-times = np.zeros((len(images)))
 
 
 def get_results(request, userdata):
     """User-defined callback function."""
     end_time = time.time()
     print('Finished picture', userdata)
-    global results
-    global times
-    results[userdata['index']] = request.output_blobs['fc_out'].buffer.copy()
+    pred_class = np.argmax(request.get_result('fc_out'))
+
+    # tmp_res[userdata['index']] = [(userdata['index'], pred_class)]
+    
+    # global tmp_res
+    # tmp_res += [(userdata['index'], pred_class)]
+
+    engine = db.create_engine('sqlite:///queue.sqlite')
+    connection = engine.connect()
+    query = db.insert(emp).values(Id=userdata['index'],
+                                  pred_class=int(pred_class))
+    connection.execute(query)
+
+    del engine
+    del connection
+
     times[userdata['index']] = (end_time - userdata['start_time']) * 1000
 
 
+# tmp_res = [0] * len(images)
+# tmp_res = []
 # Set callbacks on each job/InferRequest
+times = np.zeros((len(images)))
 infer_queue.set_infer_callback(get_results)
 
 print('Starting InferQueue...')
 start_queue_time = time.time()
 for i in range(len(images)):
-    # If advanced user would like to have control:
-    # tensor_desc = TensorDesc('FP32',
-    #                          [1, 3, images[i].shape[2], images[i].shape[3]],
-    #                          'NCHW')
-    # img_blob = Blob(tensor_desc, images[i])
     start_request_time = time.time()
     infer_queue.async_infer(inputs={'data': images[i]},
                             userdata={'index': i,
@@ -87,12 +119,13 @@ if np.all(np.array(statuses) == StatusCode.OK):
     print('Reference execution time:', ref_time)
     print('Finished InferQueue! Execution time:', queue_time)
     print('Times for each image: ', times)
+    show_table(emp)
+    results = connection.execute(db.select([emp])).fetchall()
+    metadata.drop_all(engine)  # drops all the tables in the database
     reference = get_reference(executable_network, images)
+    assert len(results) == len(images)
     for i in range(len(results)):
-        for key in executable_network.output_info:
-            # print(results[i])
-            # print(reference[i][key].buffer)
-            assert np.allclose(results[i],
-                               reference[i][key].buffer)
+        assert ref_results[i] == results[i]
 else:
+    metadata.drop_all(engine)  # drops all the tables in the database
     raise RuntimeError('InferQueue failed to finish!')
